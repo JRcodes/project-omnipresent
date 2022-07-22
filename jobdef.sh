8464bdbeac4073f7ea25db5fcd7eecc9f6df8296
Business Business Learning <business@ns-macbook-air.lan>
2022-07-22 03:00:52 -0400

    simplified apache+php install in job definition

--- jobdef.sh
+++ jobdef.sh
@@ -7,26 +7,11 @@ sudo apt upgrade -y
 
 # Install apache: check if installed if not then install
 echo "# # # # # Checking if apache2 is installed...."
-apache=$(which apache2)
+sudo apt install apache2 -y
 
-if [ -z $apache ]; then
-    echo "# # # # # Could not find a version of apache2."
-    echo "# # # # # Installing apache2...."
-    sudo apt install -y apache2
-else
-    echo "# # # # # An installation for apache2 found\n"
-fi
 # Install php: check if installed if not then install
 echo "# # # # # Checking if php is installed....\n"
-php=$(which php)
-
-if [ -z $php ]; then
-    echo "# # # # # Could not find a version of php\n"
-    echo "# # # # # Installing php....\n"
-    sudo apt install -y php
-else
-    echo "# # # # # An installation for php found\n"
-fi
+psudo apt install php -y
 
 # Remove test index and place php test file at document root
 if [ -e $DOCROOT/index.html ]; then
